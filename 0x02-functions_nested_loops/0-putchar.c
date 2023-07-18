4e0b78b92ba948849d20b1ce00ac4df638fad37c
wayneronny <itsronaldomondi@gmail.com>
2023-07-18 14:46:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include <main.h>
+#include "holberton.h"
 
 /**
- * main - Entry point of my code,
- * Return: Always 0
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
 _putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 _putchar('\n');
+
 return (0);
 }
